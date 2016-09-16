3f0971a33d1a3203e8a57ae38f1a51e376143a47
AllocatorXy <misaka20001@vip.qq.com>
2016-09-16 16:54:33 +0800

    update

--- js/js.js
+++ js/js.js
@@ -24,15 +24,22 @@ window.onload = function ()
 		timerId = setInterval(function () {
 				var speed = (oTarLeft - obj.offsetLeft)/10;
 				speed = speed>0?Math.ceil(speed):Math.floor(speed);
-				obj.style.left = obj.offsetLeft+speed+'px';
+				if (obj.offsetWidth == oTarLeft) 
+				{
+					clearInterval(timerId);
+				}
+				else
+				{
+					obj.style.left = obj.offsetLeft+speed+'px';
+				}
 			}, 15);	
+
 		var arr = [flag,timerId]; 
 		return arr;
 		//clearInterval的参数是1~n的正整数id，而js传参是传值，所以不能直接传递timer。
 		//需要把定时器的id传出去再赋给timer本身然后才能做到异步清除定时器且不混乱。
 	}
 
-	/*这里有一个小问题，前两次点击有一个块不会同步运动，待解决*/
 	oDivMov.onclick = function () 
 	{
 		arr = act(oDivMov,arr[0],iTarget1,arr[1]);
