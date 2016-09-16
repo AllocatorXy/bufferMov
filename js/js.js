e7effb79618e6c442996f60c195ec49489395aa4
AllocatorXy <misaka20001@vip.qq.com>
2016-09-16 17:51:54 +0800

    -

--- js/js.js
+++ js/js.js
@@ -7,39 +7,6 @@ window.onload = function ()
 	var oBody = document.getElementsByTagName('body')[0];
 	var arr = [0,0];var arr2 = [1,0];
 
-	function act(obj,flag,defTar,timerId) //用外部变量保存target不用动态计算的原因是：
-	{									 //这样狂点也不会因为在未完成位移的情况下飘移
-		clearInterval(timerId);
-		var oTarLeft = null;
-		if (flag === 0) 
-		{
-			oTarLeft = defTar;
-			flag = 1;
-		}
-		else
-		{
-			oTarLeft = defTar-obj.offsetWidth;
-			flag = 0;
-		}
-		timerId = setInterval(function () {
-				var speed = (oTarLeft - obj.offsetLeft)/10;
-				speed = speed>0?Math.ceil(speed):Math.floor(speed);
-				
-				if (obj.offsetLeft == oTarLeft) 
-				{
-					clearInterval(timerId);
-				}
-				else
-				{
-					obj.style.left = obj.offsetLeft+speed+'px';
-				}
-			}, 15);	
-
-		var arr = [flag,timerId]; 
-		return arr;
-		//clearInterval的参数是1~n的正整数id，而js传参是传值，所以不能直接传递timer。
-		//需要把定时器的id传出去再赋给timer本身然后才能做到异步清除定时器且不混乱。
-	}
 
 	oDivMov.onclick = function () 
 	{
@@ -52,3 +19,36 @@ window.onload = function ()
 		arr = act(oDivMov,arr[0],iTarget1,arr[1]);
 	};
 };
+function act(obj,flag,defTar,timerId) //用外部变量保存target不用动态计算的原因是：
+{									 //这样狂点也不会因为在未完成位移的情况下飘移
+	clearInterval(timerId);
+	var oTarLeft = null;
+	if (flag === 0) 
+	{
+		oTarLeft = defTar;
+		flag = 1;
+	}
+	else
+	{
+		oTarLeft = defTar-obj.offsetWidth;
+		flag = 0;
+	}
+	timerId = setInterval(function () {
+			var speed = (oTarLeft - obj.offsetLeft)/10;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			
+			if (obj.offsetLeft == oTarLeft) 
+			{
+				clearInterval(timerId);
+			}
+			else
+			{
+				obj.style.left = obj.offsetLeft+speed+'px';
+			}
+		}, 15);	
+
+	var arr = [flag,timerId]; 
+	return arr;
+	//clearInterval的参数是1~n的正整数id，而js传参是传值，所以不能直接传递timer。
+	//需要把定时器的id传出去再赋给timer本身然后才能做到异步清除定时器且不混乱。
+}
